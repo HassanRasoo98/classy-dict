56495689bb57f0fd1c08e59d1273e117a68e9667
HassanRasoo98 <hassanrasool1057@gmail.com>
2025-03-20 11:32:28 -0700

    update: project description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open('readme.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='classyDict',
     version='0.1.0',
     packages=find_packages(),
     description='A dictionary that supports dot notation access, including nested dicts.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Your Name',
     author_email='your.email@example.com',
     url='https://github.com/yourusername/classyDict',
